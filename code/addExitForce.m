3be6dcbb271735c1f6f4e53865c10e31cbdca461
Exomag <exomag1993@gmail.com>
2017-11-17 16:34:47 +0100

    fixed phi calculation

--- code/addExitForce.m
+++ code/addExitForce.m
@@ -16,7 +16,7 @@ for fi = 1:data.floor_count
             nik = data.floor(fi).img_exitC{k} - p;
             dik = norm(nik);
             nik = nik / dik;
-            phi = cos(dot(nik,e));
+            phi = dot(nik,e);
             if phi < 0
                 phi = 0;
             end
