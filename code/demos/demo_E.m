a4bbc92b00223490b7f49c63dcf4f752c903e496
Exomag <exomag1993@gmail.com>
2017-11-27 22:21:32 +0100

    fixed config path

--- code/demos/demo_E.m
+++ code/demos/demo_E.m
@@ -4,7 +4,7 @@ cd('..')
 s = rng('shuffle','twister');
 
 rng(s)
-simulate('config_demo_E-0.2');
+simulate('data/config_demo_E-0.2.conf');
 
 rng(s)
-simulate('config_demo_E-0.8');
+simulate('data/config_demo_E-0.8.conf');
