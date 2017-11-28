e92739f4f9e95aed30558bbbe76cd3dd93a1844e
Matteo Signer <matteo.signer@gmail.com>
2017-11-28 16:31:00 +0100

    Improve cost function to use pathfinding data

--- code/initEscapeRoutes.m
+++ code/initEscapeRoutes.m
@@ -7,7 +7,8 @@ for i=1:data.floor_count
     boundary_data = zeros(size(data.floor(i).img_wall));
     boundary_data(data.floor(i).img_wall) =  1;
     boundary_data(data.floor(i).img_stairs_down) = -1;
-    
+
+    data.floor(i).exit_dist = {};
     data.floor(i).img_dir_x = {};
     data.floor(i).img_dir_y = {};
     for j = 1 : length(data.floor(i).img_exit)
@@ -16,6 +17,7 @@ for i=1:data.floor_count
         exit_dist = fastSweeping(temp) * data.meter_per_pixel;
         [data.floor(i).img_dir_x{end+1}, data.floor(i).img_dir_y{end+1}] = ...
             getNormalizedGradient(temp, -exit_dist);
+        data.floor(i).exit_dist{end+1} = exit_dist;
     end
 end
 
